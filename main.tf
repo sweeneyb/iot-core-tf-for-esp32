c86a08f104d8b41e4a3a554fe7580e0e8ad72258
sweeneyb <tab126@gmail.com>
2020-12-24 23:42:50 -0500

    fully working; verified messages through the subscription on event topic

--- main.tf
+++ main.tf
@@ -26,6 +26,17 @@ resource "google_pubsub_topic" "default-telemetry" {
   project = google_project.iot-project.project_id
 }
 
+resource "google_pubsub_subscription" "cli-verification-subscription" {
+  name  = "cli-verify"
+  project = google_project.iot-project.project_id
+  topic = google_pubsub_topic.default-telemetry.name
+}
+
+resource "google_pubsub_topic" "state-events" {
+  name = "state-events"
+  project = google_project.iot-project.project_id
+}
+
 resource "google_project_service" "cloudiot" {
   project = google_project.iot-project.project_id
   service = "cloudiot.googleapis.com"
@@ -33,17 +44,21 @@ resource "google_project_service" "cloudiot" {
   disable_dependent_services = true
 }
 
-resource "google_cloudiot_registry" "test-registry" {
+resource "google_cloudiot_registry" "cloudiot-registry" {
   name     = "cloudiot-registry"
   project = google_project.iot-project.project_id
-  region = "us-central1"
+  region = var.region
 
   event_notification_configs {
     pubsub_topic_name = google_pubsub_topic.default-telemetry.id
     subfolder_matches = ""
   }
 
-    mqtt_config = {
+  state_notification_config = {
+    pubsub_topic_name = google_pubsub_topic.state-events.id
+  }
+
+  mqtt_config = {
     mqtt_enabled_state = "MQTT_ENABLED"
   }
 
@@ -53,4 +68,21 @@ resource "google_cloudiot_registry" "test-registry" {
 
   depends_on = [google_project_service.cloudiot]
 
+}
+
+resource "tls_private_key" "esp32-01" {
+  algorithm   = "RSA"
+  rsa_bits = 2048
+}
+
+resource "google_cloudiot_device" "esp32-device-01" {
+  name     = "sparkfun-thing-01"
+  registry = google_cloudiot_registry.cloudiot-registry.id
+
+  credentials {
+    public_key {
+        format = "RSA_PEM"
+        key = tls_private_key.esp32-01.public_key_pem
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "proj_id" {
   type    = string
   default = "foo"
 }
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
